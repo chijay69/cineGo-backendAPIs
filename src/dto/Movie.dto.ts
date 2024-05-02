cbc1ae073bc09daf1114ae3379a39e094a8c3bcd
chijayCPL <chijindu.alozie@crusaderpensions.com>
2024-05-02 11:28:39 +0100

    did sonething

--- src/dto/Movie.dto.ts
+++
@@ -1,8 +0,0 @@
-export class MovieResponse {
-    title: string;
-    url: string;
-    description: string;
-    year: string;
-    rating: string;
-    image: string;
-}
--- src/repository/MovieRepository.ts
+++
@@ -1,7 +0,0 @@
-import { AppDataSource } from "../data-source";
-import { EntityTarget, Repository } from "typeorm";
-import { Movie } from "../entity/Movie";
-
-export const MovieRepository = (entity: EntityTarget<Movie>): Repository<Movie> => {
-    return AppDataSource.getRepository(entity);
-};
